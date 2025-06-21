82686a834da6dffa2271dbeb8e8b1f11e8120aac
Sabbir Ahmed <sabbir.personal@proton.me>
2025-06-21 16:16:23 +0600

    Create module5assignment.dart

---
+++ module5assignment.dart
@@ -0,0 +1,28 @@
+abstract class Vehicle {
+  int _speed = 0;
+
+  void move();
+
+  void setSpeed(int speed) {
+    if (speed > 0) {
+      _speed = speed;
+    } else {
+      print("Speed must be greater than 0.");
+    }
+  }
+
+  int get speed => _speed;
+}
+
+class Car extends Vehicle {
+  @override
+  void move() {
+    print("The car is moving at $_speed km/h");
+  }
+}
+
+void main() {
+  Car myCar = Car();
+  myCar.setSpeed(80);
+  myCar.move();
+}
